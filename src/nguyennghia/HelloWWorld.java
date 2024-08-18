1dc2a8bf7357d850fbb2722da34da54a09f0d102
NguyenNghia0100 <nghianguyenduc3110@gmail.com>
2024-08-18 21:36:21 +0700

    java5

--- src/nguyennghia/HelloWWorld.java
+++ src/nguyennghia/HelloWWorld.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 public class HelloWWorld {
 	public static void main(String[] args) {
 
+		System.out.println(">> LỰA CHỌN TÍNH NĂNG <<");
+		System.out.println("++ ------------------ ++");
+		System.out.println("| 1. Cộng |");
+		System.out.println("| 2. Trừ |");
+		System.out.println("| 3. Kết thúc |");
+		System.out.println("++ ------------------ ++");
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Lựa chọn của bạn là : ");
 
-		System.out.println("Nhập tên sinh viên: ");
-		String tsv = scanner.nextLine();
+		int value = scanner.nextInt();
 
-		System.out.println("Điểm đầu: ");
-		int num1 = scanner.nextInt();
-
-		System.out.println("Điểm cuối: ");
-		int num2 = scanner.nextInt();
-
-		System.out.println("Tên sinh viên:  " + tsv + " Điểm trung bình là: " + (num1 + num2) / 2);
-		scanner.close();
+		switch (value) {
+		case 1: {
+			System.out.println("Bạn đã lựa chọn phép cộng");
+			break;
+		}
+		case 2: {
+			System.out.println("Bạn đã lựa chọn phép trừ");
+			break;
+		}
+		default:
+			System.out.println("Bạn đã lựa chọn thoát chương trình");
+			System.exit(0);
+		}
 
 	}
 }
