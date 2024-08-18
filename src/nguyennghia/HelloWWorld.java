55c5eacb1898b350c0bcb7cb675db9274971b3c7
NguyenNghia0100 <nghianguyenduc3110@gmail.com>
2024-08-18 22:10:42 +0700

    java7

--- src/nguyennghia/HelloWWorld.java
+++ src/nguyennghia/HelloWWorld.java
@@ -1,11 +1,21 @@
 package nguyennghia;
 
+import java.util.Arrays;
+
 public class HelloWWorld {
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 5; i++) {
-			System.out.println(i);
+		int[] a = { 10, 5, 20, 30, 1, 3, 69, 12, 2 };
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
 		}
+		System.out.println("a = " + Arrays.toString(a));
 
 	}
 }
